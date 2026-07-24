bdd3962ad5c6138ba5bedf4c0b90ff89ccfcd01c
Brayden <coghill30@gmail.com>
2026-07-23 20:02:10 -0700

    Updates

--- assets/Hero3D-D7soZ9c2.js
+++ assets/Hero3D-D7soZ9c2.js
@@ -1,4 +1,4 @@
-import{r as Yx,g as Zx,R as $x,a as rt,j as Tn,u as A1}from"./main-BpRpLFTu.js";/**
+import{r as Yx,g as Zx,R as $x,a as rt,j as Tn,u as A1}from"./main-BDKfVm3X.js";/**
  * @license
  * Copyright 2010-2026 Three.js Authors
  * SPDX-License-Identifier: MIT
